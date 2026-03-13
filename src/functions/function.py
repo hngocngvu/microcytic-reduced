42602e40a79a3a4b5120380b1d665734509aa305
hngocngvu <hongngocnguyenvu.work@gmail.com>
2026-03-13 14:13:03 +0000

    Add files following the instructions of the hospital

---
+++ src/functions/function.py
@@ -0,0 +1,85 @@
+import os
+from typing import Dict, List, Optional, Tuple
+
+cwd= os.getcwd()
+BASE_DIR= os.path.join(cwd,"..","..")
+if BASE_DIR not in os.sys.path:
+    os.sys.path.append(BASE_DIR)
+
+import math
+
+def cal_mentzer(mcv, rbc):
+    return mcv/rbc
+
+def stfr_ferritin_index(stfr, ferritin, thres, labels):
+    diagnosis: str 
+    reason: str
+    idx= 0
+
+    if ferritin is not None and stfr is not None and ferritin >0 and ferritin !=1: 
+        idx= stfr / math.log10(ferritin)
+
+    if idx > thres['stfr_fer_idx'][1]:
+        diagnosis= labels[1]
+        reason= ""
+                        
+    elif idx < thres['stfr_fer_idx'][0]:
+        diagnosis= labels[3]
+        reason= ""
+
+
+    else:
+        reason= "Theo dõi thêm (thử sắt)"
+        diagnosis= labels[2]
+        reason= ""
+
+
+    return diagnosis, reason
+
+def cal_mentzer(mcv, rbc):
+    return mcv/rbc
+
+
+def diendihst(data, thres, labels):
+    diagnosis = None
+    reason = None
+
+    if data.dotbiengen:
+        if data.hbbart is not None and data.hbbart > thres["hbbart"]: 
+            diagnosis = labels[5]
+            reason= ""
+
+
+        if data.hba2 is not None and data.hba2 > thres["hba2"]:
+            diagnosis = labels[6]
+            reason= ""
+
+
+        if (data.hb_other is not None and data.hb_other > thres["hb_other"]) or \
+           (data.hbs is not None and data.hbs > thres["hbs"]) or \
+           (data.hbe is not None and data.hbe > thres["hbe"]):
+            diagnosis = labels[0]
+            reason= ""
+
+
+    else: 
+        reason = "Hội chẩn chuyên gia kèm tiền sử gia đình."
+
+    return diagnosis, reason
+
+
+
+
+
+                    
+                
+
+
+                    
+
+        
+
+
+    
+            
+
---
+++ src/models/classifier.py
@@ -0,0 +1,115 @@
+import os 
+
+# mentzer= mcv/rbc
+# tsat%= fe*100%/tibc or fe*70.9/transferin
+# stfr/fer_idx= stfr/log(ferritin)
+
+cwd= os.getcwd()
+BASE_DIR= os.path.join(cwd,"..","..")
+if BASE_DIR not in os.sys.path:
+    os.sys.path.append(BASE_DIR)
+
+from src.dataclass.schema import Input, Output, Config
+from src.functions.function import stfr_ferritin_index, cal_mentzer, diendihst
+
+class Classifier():
+    def __init__(self, data, config):
+        self.data= data
+        self.config= config
+
+    def classify(self) -> Output:
+        data= self.data 
+        thres= self.config.thres
+        labels= self.config.labels
+
+        reasons= []
+        diagnoses= []
+
+
+        FIELDS = ["mcv", "hb", "crp", "ferritin", "transferrin", "stfr", "rdw", "rbc", "dotbiengen", "hbbart", "hba2", "hb_other", "hbs", "hbe"]
+
+        if any(getattr(data, f) is not None for f in FIELDS):
+            if data.dotbiengen:
+                d, r= diendihst(data, thres, labels)
+                diagnoses.append(d)
+                reasons.append(r)
+            
+    
+            if data.mcv < 80: 
+                reasons.append("Thể tích trung bình hồng cầu nhỏ hơn bình thường.")
+
+
+            if data.hb < thres["hb"][data.gender]:
+                if data.ferritin is None: pass
+
+                elif data.ferritin < thres["ferritin"][0]:
+                    # trả về IDA
+                    diagnoses.append(labels[1])
+                
+                else: 
+                    if data.crp is None: pass
+                    elif data.crp > thres['crp']:
+                        if data.ferritin is None: pass
+
+                        elif data.ferritin < thres["ferritin"][1]:
+                            if data.transferrin is not None or data.stfr is not None or data.ferritin is not None:
+
+                                if data.transferrin < thres['transferrin'][0]:
+                                    # xét tiền sử, scoring dùng cho đoạn này, chưa biết vì sơ đồ không đề cập
+                                    reasons.append("Cần dựa vào tiền sử để đưa ra kết luận chính xác.")
+                                    diagnoses.append(labels[4])
+                                    
+                                elif data.transferrin > thres["transferrin"][1]:
+                                    # trả về ACD
+                                    diagnoses.append(labels[3])
+                                    reasons.append(r)
+                                else: 
+                                    d,r= stfr_ferritin_index(data.stfr, data.ferritin, thres, labels)
+                                    diagnoses.append(d)
+                                    reasons.append(r)
+                        else: 
+                            d, r= stfr_ferritin_index(data.stfr, data.ferritin, thres, labels)
+                            diagnoses.append(d)
+                            reasons.append(r)
+
+
+
+                    else:
+                        if data.transferrin is None: pass
+
+                        elif data.transferrin < thres['transferrin'][0]:
+                            # trả về IDA
+                            diagnoses.append(labels[1])
+
+                        elif data.transferrin > thres['transferrin'][1]:
+                            if not data.dotbiengen:
+                                reasons.append("Cần điện di huyết sắc tố để kiểm tra đột biến gen.")
+                            else: 
+                                d, r= diendihst(data, thres, labels)
+                                diagnosis.append(d)
+                                reasons.append(r)
+
+                        else: 
+                            reasons.append("Gợi ý IDA + tiền sử")
+                            diagnosis.append(labels[1])
+
+
+            else: 
+                if data.rdw is not None or data.mcv is not None or data.rbc is not None or data.ferritin is not None:
+
+                    if data.ferritin < thres["ferritin"][0] and (data.rdw < thres["rdw"] or cal_mentzer(data.mcv, data.rbc) > thres['mentzer']):
+                        # trả về IDA
+                        diagnosis.append(labels[1])
+
+                    else:
+                        if not data.dotbiengen:
+                            reasons.append("Cần điện di huyết sắc tố để kiểm tra đột biến gen.")
+                        else: 
+                            d, r= diendihst(data, thres, labels)
+                            diagnoses.append(d)
+                            reasons.append(r)
+
+        return Output(diagnoses= diagnoses, reasons=reasons)
+
+
+
