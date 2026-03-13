c8311324580f93d3c99fe4905fb042278af25d09
hngocngvu <hongngocnguyenvu.work@gmail.com>
2026-03-13 14:13:03 +0000

    Add files following the instructions of the hospital

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# microcytic-reduced
+# Multi-Label Classification of Microcytic Anemia–Related Diagnoses Using Reduced Laboratory Parameters
+
+Microcytic anemia is commonly associated with several underlying conditions, including iron deficiency anemia (IDA), anemia of chronic disease (ACD) and thalassemia. Although these disorders share similar hematological characteristics, their clinical management and treatment strategies differ substantially.
+
+In hospitals, the diagnostic process involves multiple laboratory tests, which can provide overlapping information. Not only is it expensive for laboratories to run but it also affects to patients since they have to bear the additional costs. 
+
+By leveraging available laboratory data, it is possible to identify a subset of key diagnostic indicators that can effectively distinguish between different causes of microcytic anemia. Optimizing the selection of laboratory parameters may help maintain diagnostic accuracy while reducing the number of required tests. This approach can improve diagnostic efficiency, minimize patient burden, and support more cost-effective clinical decision-making.
+
+The dataset is provided by the hospital, therefore it is privately used. No analysis of this dataset is uploaded in this repository.
+
+## Dependency Instalation
+
+```
+pip install -r requirements.txt
+```
+
+## Methods
+
+- Rule-based: provided by the hospital
+
+- Machine learning & Deep learning based: may develop in the future if the provided dataset is larger 
---
+++ src/dataclass/schema.py
@@ -0,0 +1,84 @@
+from typing import Dict, List, Optional, Tuple
+from dataclasses import dataclass, field
+
+@dataclass
+class Input():
+    gender: str 
+    kinh_nguyet: Optional[bool]= False
+    da_day:  Optional[bool]= False
+    tri:  Optional[bool]= False
+    pregnant:  Optional[bool]= False
+    diet:  Optional[bool]= False
+
+    man_tinh:  Optional[bool]= False
+    cancer:  Optional[bool]= False
+    phau_thuat:  Optional[bool]= False
+
+    rbc: Optional[float]= None
+    hb: Optional[float]= None
+    mcv: Optional[float]= None
+    mchc: Optional[float]= None
+    rdw: Optional[float]= None
+    ret_he: Optional[float]= None
+
+    fe: Optional[float]= None
+    ferritin: Optional[float]= None
+    transferrin: Optional[float]= None
+    tibc: Optional[float]= None
+    stfr: Optional[float]= None
+    crp: Optional[float]= None
+
+    dotbiengen: Optional[bool]= False
+    hba: Optional[float]= None
+    hba2: Optional[float]= None
+    hbf: Optional[float]= None
+    hbh: Optional[float]= None
+    hbe: Optional[float]= None
+    hbc: Optional[float]= None
+    hbs: Optional[float]= None
+    hbbart: Optional[float]= None
+    hb_other: Optional[float]= None
+
+
+@dataclass
+class Output():
+    diagnoses: Optional[List[str]]
+    reasons: Optional[List[str]]
+    # confidence: Optional[float]
+    # scores: Optional[Dict[str, float]]
+
+@dataclass
+class Config():
+    # weights: dict= field(default_factory= lambda: {})
+    thres: dict= field(default_factory= lambda: {
+        'hb': {"pregnant": 110, 
+                "female": 120, 
+                "male": 130}, #thresholds cho PN có thai, nữ và nam
+
+        'ferritin': [30, 100],
+        'rdw': 0.15,
+        'mentzer': 13,
+        'hbbart': 0,
+        'hbh': 0,
+        'hba2': 0.035,
+        'hbf': 0.01,
+        'hbe': 0,
+        'hbs': 0,
+        'hbc': 0,
+        'hb_other': 0,
+        'transferrin': [0.2, 0.3],
+        'crp': 5, 
+        'stfr_fer_idx' : [1, 2]
+    })
+
+    labels= [
+            'Thalassemia',
+            "IDA",
+            "CRNN",
+            "ACD",
+            "IDA/ACD",
+            'alpha-Thalassemia',
+            'beta-Thalassemia'
+        ]
+
+
