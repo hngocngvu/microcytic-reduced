e3401f41f2d344e47f2ec06d2e75caf4ce32725c
hngocngvu <hongngocnguyenvu.work@gmail.com>
2026-03-13 17:02:10 +0000

    Debug successfully

--- demo-web/main.py
+++ demo-web/main.py
@@ -10,7 +10,6 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-print(BASE_DIR)
 
 from src.dataclass.schema import Input, Output, Config
 from src.models.classifier import Classifier
