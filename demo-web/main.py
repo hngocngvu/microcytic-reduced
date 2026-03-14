03ce7ae5b576222c48e1833a27ed37b13165b4f8
hngocngvu <hongngocnguyenvu.work@gmail.com>
2026-03-14 13:52:02 +0000

    Edit functions and web UI based on suggestion

--- demo-web/main.py
+++ demo-web/main.py
@@ -14,48 +14,59 @@ if BASE_DIR not in sys.path:
 from src.dataclass.schema import Input, Output, Config
 from src.models.classifier import Classifier
 
-st.title("Hệ thống chẩn đoán bệnh về hồng cầu nhỏ")
-
+st.set_page_config(layout="wide")
+st.markdown(
+    "<h1 style='text-align: center;'>Hệ thống chẩn đoán bệnh về hồng cầu nhỏ</h1>",
+    unsafe_allow_html=True
+)
 with st.form("form"):
-    st.subheader("Thông tin chung và tiền sử")
-    gender= st.text_area("Giới tính (Nam, Nữ)")
-    kinh_nguyet= st.checkbox("Kinh nguyệt nhiều, kéo dài")
-    da_day= st.checkbox("Viêm loét dạ dày, tá tràng hoặc tiền sử cẳt dạ dày, tá tràng")
-    tri= st.checkbox("Trĩ chảy máu")
-    pregnant= st.checkbox("Có thai")
-    diet= st.checkbox("Chế độ ăn kiêng, ăn chay")
+    col1, col2, col3, col4 = st.columns(4)
 
-    st.subheader("Tiền sử liên quan đến thiếu máu trong tình trạng viêm")
-    man_tinh= st.checkbox("Có bệnh nhiễm trùng/viêm mãn tính đang điều trị: gout, viêm khớp, nhiễm khuẩn, lupus ban đỏ hệ thống, bệnh thận mạn, viêm gan mạn ,...")
-    cancer= st.checkbox("Ung thư đang điều trị")
-    phau_thuat= st.checkbox("Sau phẫu thuật lớn, bỏng, chấn thương")
+    with col1:
+        st.subheader("Thông tin chung và tiền sử")
+        gender = st.selectbox(
+        "Giới tính",
+        ["Nam", "Nữ"])
+        kinh_nguyet= st.checkbox("Kinh nguyệt nhiều, kéo dài")
+        da_day= st.checkbox("Viêm loét dạ dày, tá tràng hoặc tiền sử cẳt dạ dày, tá tràng")
+        tri= st.checkbox("Trĩ chảy máu")
+        pregnant= st.checkbox("Có thai")
+        diet= st.checkbox("Chế độ ăn kiêng, ăn chay")
 
-    st.subheader("Các đặc điểm hồng cầu")
-    hb= st.number_input("Hemoglobin (Hb)")
-    mcv= st.number_input("MCV")
-    mchc= st.number_input("MCHC")
-    rbc= st.number_input("RBC")
-    rdw= st.number_input("RDW")
-    ret_he= st.number_input("Ret-He")
+        st.subheader("Tiền sử liên quan đến thiếu máu trong tình trạng viêm")
+        man_tinh= st.checkbox("Có bệnh nhiễm trùng/viêm mãn tính đang điều trị: gout, viêm khớp, nhiễm khuẩn, lupus ban đỏ hệ thống, bệnh thận mạn, viêm gan mạn ,...")
+        cancer= st.checkbox("Ung thư đang điều trị")
+        phau_thuat= st.checkbox("Sau phẫu thuật lớn, bỏng, chấn thương")
 
-    st.subheader("Chỉ số hóa sinh máu")
-    fe= st.number_input("Định lượng Sắt huyết thành (Fe)")
-    ferritin= st.number_input("Định lượng Ferritin")
-    transferrin= st.number_input("Định lượng Transferrin")
-    tibc= st.number_input("TIBC (khả năng gắn sắt toàn phần: máy đo)")
-    stfr= st.number_input("Nồng độ thụ thể transferrin hòa tan")    
-    crp= st.number_input("CRP")
+    with col2:
+        st.subheader("Các đặc điểm hồng cầu")
+        hb= st.number_input("Hemoglobin (Hb)")
+        mcv= st.number_input("MCV")
+        mchc= st.number_input("MCHC")
+        rbc= st.number_input("RBC")
+        rdw= st.number_input("RDW")
+        ret_he= st.number_input("Ret-He")
 
-    st.subheader("Xét nghiệm di huyết sắt tố và đột biến gen")
-    hba= st.number_input("HbA")
-    hba2= st.number_input("HbA2")
-    hbf= st.number_input("HbF")
-    hbh= st.number_input("HbH")
-    hbe= st.number_input("HbE")
-    hbc= st.number_input("HbC")
-    hbs= st.number_input("HbS")
-    hb_other= st.number_input("Chỉ số Hb khác nếu có")
-    dotbiengen= st.checkbox("Đột biến gen thalassemia")
+    with col3: 
+        st.subheader("Chỉ số hóa sinh máu")
+        fe= st.number_input("Định lượng Sắt huyết thành (Fe)")
+        ferritin= st.number_input("Định lượng Ferritin")
+        transferrin= st.number_input("Định lượng Transferrin")
+        tibc= st.number_input("TIBC (khả năng gắn sắt toàn phần: máy đo)")
+        stfr= st.number_input("Nồng độ thụ thể transferrin hòa tan")    
+        crp= st.number_input("CRP")
+
+    with col4: 
+        st.subheader("Xét nghiệm di huyết sắt tố và đột biến gen")
+        hba= st.number_input("HbA")
+        hba2= st.number_input("HbA2")
+        hbf= st.number_input("HbF")
+        hbh= st.number_input("HbH")
+        hbe= st.number_input("HbE")
+        hbc= st.number_input("HbC")
+        hbs= st.number_input("HbS")
+        hb_other= st.number_input("Chỉ số Hb khác nếu có")
+        dotbiengen= st.checkbox("Đột biến gen thalassemia")
 
     submitted = st.form_submit_button("Submit")
     if submitted:
